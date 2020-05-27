e0781f1110b0d181b3e59ad8c362fb8869a7fc1f
thiagocossta <35429223+thiagocossta@users.noreply.github.com>
2020-05-27 00:47:30 -0300

    refact(database): fixing credential error

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://root:1q3e2w5t4rmmm@serverj-o7jm9.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://root:root@serverj-o7jm9.mongodb.net/test?retryWrites=true&w=majority',
 {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 mongoose.Promise = global.Promise;
 
